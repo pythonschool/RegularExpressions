468b02515e6000e774b87b79e9b638bad78299ca
Adam McNicol <adammcnicol@mac.com>
2012-12-19 19:56:33 +0000

    changed to verbose expression
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- post_codes.py
+++ post_codes.py
@@ -2,7 +2,23 @@
 
 import re
 
-pattern = "[A-Z]([A-Z](\d{1,2}|\d[A-Z])|\d{1,2})\s\d[A-Z]{2}"
+pattern = """
+                [A-Z]               #a single letter
+                                    #followed by either:
+                    (                   
+                    [A-Z]           #an another letter
+                                    #and either:
+                        (
+                        \d{1,2}     #one or two numbers
+                        |           #or
+                        \d[A-Z]     #a single number followed by a single letter
+                        )   
+                    |               #or
+                    \d{1,2}         #one or two numbers
+                    )
+                \s\d                #space followed by a single number
+                [A-Z]{2}            #two letters
+            """
 
 finished = False
 
@@ -11,7 +27,7 @@ while not finished:
     if len(post_code) == 0:
         finished = True
     else:
-        if re.match(pattern,post_code):
+        if re.match(pattern,post_code,re.VERBOSE):
             print("Valid post code")
         else:
             print("Invalid post code")
