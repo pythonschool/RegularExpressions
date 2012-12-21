b001bafc15ebc2c331e82bed1ecff0b1be735f42
Adam McNicol <adammcnicol@mac.com>
2012-12-21 19:55:19 +0000

    verbose phone number added
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- phone_number.py
+++ phone_number.py
@@ -2,7 +2,35 @@
 
 import re
 
-pattern = "\(0(1(\d{3}\)\s\d{6}|(\d1|1\d)\)\s\d{3}\s\d{4})|2\d\)\s\d{4}\s\d{4})"
+pattern = """\(0                   #open bracket followed by a zero
+                                     #then either:
+               (
+                   1                 #1
+                                     #followed by either:
+                   (
+                       \d{3}         #3 digits
+                       \)            #close bracket
+                       \s            #single space
+                       \d{6}         #6 digits
+                   |                 #or
+                       (
+                           \d1       #a single digit followed by a 1
+                       |             #or
+                           1\d       #a 1 followed by a single digit
+                       )
+                       \)            #a close bracket
+                       \s            #a single space
+                       \d{3}         #3 digits
+                       \s            #a single space
+                       \d{4}         #4 digits
+                   )
+               |                     #or
+                   2\d\)             #2 followed by a digit and a close bracket
+                   \s                #a single space
+                   \d{4}             #4 digits
+                   \s                #a single space
+                   \d{4}             #4 digits
+               )$"""
 
 finished = False
 
@@ -11,7 +39,7 @@ while not finished:
     if len(phone_number) == 0:
         finished = True
     else:
-        if re.match(pattern,phone_number):
+        if re.match(pattern,phone_number,re.VERBOSE):
             print("Valid phone number")
         else:
             print("Invalid phone number")
